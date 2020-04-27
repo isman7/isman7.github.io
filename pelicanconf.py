bce543373bb385f5d5888122f14f19d692501b48
Ismael Benito <ismael.benito.altamirano@gmail.com>
2020-04-27 23:45:16 +0200

    New theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/Madrid'
 
 DEFAULT_LANG = 'es'
 
+THEME = "./themes/attila"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
